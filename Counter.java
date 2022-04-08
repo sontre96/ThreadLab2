9b3f1c2e55d0cee8620184c0de332abcd5c76596
sontre96 <sontre96@gmail.com>
2022-04-08 17:02:52 +0700

    first commit

---
+++ Counter.java
@@ -0,0 +1,27 @@
+
+
+public class Counter {
+    int value;
+
+    public synchronized   void count() {
+        value = 0;
+        while ( value < 10){
+            System.out.println("Count: "+ (++value));
+            pause(300);
+        }
+    }
+
+    public synchronized   void countdown(){
+        value =10;
+        while (value >=0){
+            System.out.println("Countdown: " + (value--));
+            pause(200);
+        }
+    }
+
+    private void pause(long time){
+        try {
+            Thread.sleep(time);
+        }catch (InterruptedException e){}
+    }
+}
